bd9ba6fb67c462e5fe623f97f29a32fc4dd38c83
AGUSTIN ADOLFO MELLA ZAPATA <agustin.mella@inacapmail.cl>
2021-11-07 21:38:12 -0300

    first commit

---
+++ Seccion 1. Nivelacion/functions.js
@@ -0,0 +1,56 @@
+/////////////////////////////////////////////////////// PARTE 1 /////////////////////////////////////////////////////
+
+// //Forma declarativa de una función en JS.
+// //El argumento puede recibir cualquier variable.
+// function NombreFuncion(){}
+
+// //Llamar a una función.
+// function saludar(){
+//     console.log('Saludo')
+// }
+// saludar()
+
+// //Arrow functions, o funciones de flecha.
+// const saludo = () => {
+//     console.log('Saludar desde Arrow Functions')
+// }
+// saludo()
+
+// //También puede usar arrow functions en una sola línea
+// const saludo2 = () => console.log('Saludar desde Arrow Functions en una sola línea') 
+// saludo2()
+
+/////////////////////////////////////////////////////// PARTE 2 /////////////////////////////////////////////////////
+
+// //Ejemplo 1 de argumentos sin definición
+// const saludar = (a, b, c) =>{
+//     console.log(a)
+//     console.log(b)
+//     console.log(c)
+// }
+// saludar(3, 'Manuel', true)
+
+// //Ejemplo 2 de argumentos sin definición
+// const saludar2 = (a, b, c) =>{
+//     return a + b + c
+// }
+// console.log(saludar2(3, 'Manuel', true))
+
+// //Ejemplo 3 de argumentos sin definición
+// const saludar3 = (a, b, c) =>{
+//     return a + b + c
+// }
+// console.log(saludar3(3, 4, 2))
+
+/////////////////////////////////////////////////////// PARTE 3 /////////////////////////////////////////////////////
+
+//Curry functions
+const func1 = (dato) => {
+    return dato
+}
+
+const func2 = (d) => {
+    console.log(d)
+}
+
+func2(func1('Manuel'))
