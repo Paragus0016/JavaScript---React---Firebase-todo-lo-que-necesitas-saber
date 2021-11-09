a0c969e75a58b953ba545eca6c8e85d53001ae49
AGUSTIN ADOLFO MELLA ZAPATA <agustin.mella@inacapmail.cl>
2021-11-09 14:23:20 -0300

    1.3 Variables

---
+++ Seccion 1. Nivelacion/variables.js
@@ -0,0 +1,31 @@
+// Numero - Number
+
+// Texto - String
+// Arrays - Colecciones [1,2,3,4]
+// Objetos - {
+//     nombre: 'Manuel',
+//     Apellido: 'Muñoz'
+// }
+// boolean - true - false
+
+//var está en desuso, se usa más let o const
+var nombre = 'Manuel'
+
+let nombre1 = 'Manuel'
+nombre1 = 32
+console.log(nombre1)
+
+//Se usa const para cambiar el valor de variable a constante.
+
+const nombres = 'Miguel'
+//nombres = 32 //Aquí la consola dará error indicando que la constante no se puede cambiar.
+console.log(nombres)
+
+const coleccion = [1,2,3,4,56]
+console.log(coleccion)
+
+const objeto = {
+    nombre: 'Manuel',
+    Apellido: 'Muñoz'
+}
+console.log(objeto)
