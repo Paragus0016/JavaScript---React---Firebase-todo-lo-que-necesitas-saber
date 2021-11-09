a3a887f6fadb271e2360e6411ac207f35e565878
AGUSTIN ADOLFO MELLA ZAPATA <agustin.mella@inacapmail.cl>
2021-11-09 17:44:37 -0300

    1.4 Template Strings

---
+++ Seccion 1. Nivelacion/template.js
@@ -0,0 +1,8 @@
+const nombre = 'Manuel'
+const apellido = 'Muñoz'
+
+console.log('El nombre es ' +nombre+ ' y el apellido es ' + apellido)
+
+//Con template string, tienen que usarse acentos invertidos `bla bla bla`
+//Para usar las variables/constantes hay que llamarlas con ${variable/constante}
+console.log(`El nombre es ${nombre} y el apellido es ${apellido}`)
